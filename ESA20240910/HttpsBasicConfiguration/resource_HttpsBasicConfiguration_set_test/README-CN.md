68ba1e060e28b632dd5e813f81da0091c3f95ea7
Alibaba Cloud SDK <sdk-team@alibabacloud.com>
2025-11-10 10:11:34 +0000

    Upload HttpsBasicConfiguration SDK code samples, jobId: d41438d2-93b6-4437-ab13-eea374f4bf7b.

---
+++ ESA20240910/HttpsBasicConfiguration/resource_HttpsBasicConfiguration_set_test/README-CN.md
@@ -0,0 +1,49 @@
+#### 项目简介
+该项目是在阿里云产品ESA（版本：2024-09-10）服务的 `HttpsBasicConfiguration` 资源下「创建、更新和删除 HTTPS 基本配置」的完整工程示例。此示例向您展示如何：
+- 创建、更新和删除 HTTPS 基本配置。
+
+#### 注意事项
+- **操作费用**：
+  - 运行示例代码可能对当前账号发起线上资源操作产生费用，请小心操作！
+
+- **依赖关系**：
+  - 资源【HTTPS 基本配置】依赖于资源【站点】，因此在创建 HTTPS 基本配置之前必须先成功创建站点。
+  - 资源【站点】依赖于资源【套餐】，因此在创建站点之前需要先成功购买套餐。
+
+- **异步操作**：
+  - 新购套餐、创建站点均为异步操作，需等待其状态更新为指定状态后再进行下一步操作。
+
+#### 工作流程
+1. **初始化客户端**
+   - 要运行此示例，您必须首先配置您的凭证，如[凭证配置](https://help.aliyun.com/zh/sdk/developer-reference/v2-manage-php-access-credentials)中所述。创建一个客户端实例。
+   
+2. **新购套餐**
+   - 调用 [PurchaseRatePlan](https://api.aliyun.com/api/ESA/2024-09-10/PurchaseRatePlan) 接口购买一个新的资源套餐，并等待其状态变为“running”。
+
+3. **创建站点**
+   - 在成功购买套餐后，调用 [CreateSite](https://api.aliyun.com/api/ESA/2024-09-10/CreateSite) 接口创建一个新的站点，并等待其状态变为“pending”。
+
+4. **创建 HTTPS 基本配置**
+   - 在站点创建成功后，调用 [CreateHttpsBasicConfiguration](https://api.aliyun.com/api/ESA/2024-09-10/CreateHttpsBasicConfiguration) 接口创建一个新的 HTTPS 基本配置。
+
+5. **更新 HTTPS 基本配置**
+   - 调用 [UpdateHttpsBasicConfiguration](https://api.aliyun.com/api/ESA/2024-09-10/UpdateHttpsBasicConfiguration) 接口更新已创建的 HTTPS 基本配置。
+
+6. **删除 HTTPS 基本配置**
+   - 最后，调用 [DeleteHttpsBasicConfiguration](https://api.aliyun.com/api/ESA/2024-09-10/DeleteHttpsBasicConfiguration) 接口删除之前创建的 HTTPS 基本配置，完成整个生命周期管理。
+
+#### 执行步骤
+- *最低要求 PHP 5.6*
+- *必须在系统上[全局安装 Composer](https://getcomposer.org/doc/00-intro.md?spm=api-workbench.SDK%20Document.0.0.206f726ceIMZ36#globally)*
+- *注意：执行 composer 安装 SDK 的 PHP 版本要小于或等于实际运行时的 PHP 版本。 例如，在 PHP7.2 环境下安装 SDK 后生成 vendor 目录，只能在 PHP7.2 以上版本使用，如果拷贝到 PHP5.6 环境下使用，会出现依赖版本不兼容问题。*
+>*一些用户可能由于网络问题无法安装，可以通过以下命令切换为阿里云 Composer 全量镜像。*
+```sh
+composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
+```
+- *执行命令*
+```sh
+composer install && php src/Sample.php
+```
+
+#### 更多示例
+更多示例请查看 https://github.com/aliyun/alibabacloud-php-sdk-samples
---
+++ ESA20240910/HttpsBasicConfiguration/resource_HttpsBasicConfiguration_set_test/src/Sample.php
@@ -0,0 +1,243 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+ 
+namespace AlibabaCloud\CodeSample;
+use AlibabaCloud\SDK\ESA\V20240910\ESA;
+use Darabonba\OpenApi\Models\Config;
+use AlibabaCloud\Credentials\Credential;
+use AlibabaCloud\SDK\ESA\V20240910\Models\PurchaseRatePlanResponseBody;
+use AlibabaCloud\Dara\Util\Console;
+use AlibabaCloud\SDK\ESA\V20240910\Models\PurchaseRatePlanRequest;
+use AlibabaCloud\SDK\ESA\V20240910\Models\DescribeRatePlanInstanceStatusRequest;
+use AlibabaCloud\Dara\Exception\DaraException;
+use AlibabaCloud\Tea\Utils\Utils;
+use AlibabaCloud\SDK\ESA\V20240910\Models\CreateSiteResponseBody;
+use AlibabaCloud\SDK\ESA\V20240910\Models\CreateSiteRequest;
+use AlibabaCloud\SDK\ESA\V20240910\Models\GetSiteRequest;
+use AlibabaCloud\SDK\ESA\V20240910\Models\CreateHttpsBasicConfigurationResponseBody;
+use AlibabaCloud\SDK\ESA\V20240910\Models\CreateHttpsBasicConfigurationRequest;
+use AlibabaCloud\SDK\ESA\V20240910\Models\UpdateHttpsBasicConfigurationRequest;
+use AlibabaCloud\SDK\ESA\V20240910\Models\DeleteHttpsBasicConfigurationRequest;
+class Sample {
+
+
+  /**
+   * @remarks
+   * Init Client
+   * @return ESA
+   */
+  static public function createESA20240910Client()
+  {
+    $config = new Config([ ]);
+    $config->credential = new Credential(null);
+    // Endpoint please refer to https://api.aliyun.com/product/ESA
+    $config->endpoint = 'esa.cn-hangzhou.aliyuncs.com';
+    return new ESA($config);
+  }
+
+  /**
+   * @param ESA $client
+   * @return PurchaseRatePlanResponseBody
+   */
+  static public function ratePlanInst($client)
+  {
+    Console::info('Begin Call PurchaseRatePlan to create resource');
+    $purchaseRatePlanRequest = new PurchaseRatePlanRequest([
+      'type' => 'NS',
+      'chargeType' => 'PREPAY',
+      'autoRenew' => false,
+      'period' => 1,
+      'coverage' => 'overseas',
+      'autoPay' => true,
+      'planName' => 'high',
+    ]);
+    $purchaseRatePlanResponse = $client->purchaseRatePlan($purchaseRatePlanRequest);
+    $describeRatePlanInstanceStatusRequest = new DescribeRatePlanInstanceStatusRequest([
+      'instanceId' => $purchaseRatePlanResponse->body->instanceId,
+    ]);
+    $currentRetry = 0;
+    $delayedTime = 10000;
+    $interval = 10000;
+
+    while ($currentRetry < 10) {
+      try {
+        $sleepTime = 0;
+        if ($currentRetry == 0) {
+          $sleepTime = $delayedTime;
+        } else {
+          $sleepTime = $interval;
+        }
+
+        Console::info('Polling for asynchronous results...');
+        usleep($sleepTime * 1000);
+      } catch (DaraException $error) {
+        throw new DaraException([
+          'message' => $error->message,
+        ]);
+      }      
+      $describeRatePlanInstanceStatusResponse = $client->describeRatePlanInstanceStatus($describeRatePlanInstanceStatusRequest);
+      $instanceStatus = $describeRatePlanInstanceStatusResponse->body->instanceStatus;
+      if ($instanceStatus === 'running') {
+        Console::info('Call PurchaseRatePlan success, response: ');
+        Console::info(Utils::toJSONString($purchaseRatePlanResponse));
+        return $purchaseRatePlanResponse->body;
+      }
+
+      $currentRetry++;
+    }
+    throw new DaraException([
+      'message' => 'Asynchronous check failed',
+    ]);
+  }
+
+  /**
+   * @param PurchaseRatePlanResponseBody $ratePlanInstResponseBody
+   * @param ESA $client
+   * @return CreateSiteResponseBody
+   */
+  static public function site($ratePlanInstResponseBody, $client)
+  {
+    Console::info('Begin Call CreateSite to create resource');
+    $createSiteRequest = new CreateSiteRequest([
+      'siteName' => 'gositecdn.cn',
+      'instanceId' => $ratePlanInstResponseBody->instanceId,
+      'coverage' => 'overseas',
+      'accessType' => 'NS',
+    ]);
+    $createSiteResponse = $client->createSite($createSiteRequest);
+    $getSiteRequest = new GetSiteRequest([
+      'siteId' => $createSiteResponse->body->siteId,
+    ]);
+    $currentRetry = 0;
+    $delayedTime = 60000;
+    $interval = 10000;
+
+    while ($currentRetry < 5) {
+      try {
+        $sleepTime = 0;
+        if ($currentRetry == 0) {
+          $sleepTime = $delayedTime;
+        } else {
+          $sleepTime = $interval;
+        }
+
+        Console::info('Polling for asynchronous results...');
+        usleep($sleepTime * 1000);
+      } catch (DaraException $error) {
+        throw new DaraException([
+          'message' => $error->message,
+        ]);
+      }      
+      $getSiteResponse = $client->getSite($getSiteRequest);
+      $status = $getSiteResponse->body->siteModel->status;
+      if ($status === 'pending') {
+        Console::info('Call CreateSite success, response: ');
+        Console::info(Utils::toJSONString($createSiteResponse));
+        return $createSiteResponse->body;
+      }
+
+      $currentRetry++;
+    }
+    throw new DaraException([
+      'message' => 'Asynchronous check failed',
+    ]);
+  }
+
+  /**
+   * @param CreateSiteResponseBody $siteResponseBody
+   * @param ESA $client
+   * @return CreateHttpsBasicConfigurationResponseBody
+   */
+  static public function httpsCfg($siteResponseBody, $client)
+  {
+    Console::info('Begin Call CreateHttpsBasicConfiguration to create resource');
+    $createHttpsBasicConfigurationRequest = new CreateHttpsBasicConfigurationRequest([
+      'siteId' => $siteResponseBody->siteId,
+      'ruleEnable' => 'on',
+      'https' => 'on',
+      'rule' => 'true',
+      'ruleName' => 'test2',
+    ]);
+    $createHttpsBasicConfigurationResponse = $client->createHttpsBasicConfiguration($createHttpsBasicConfigurationRequest);
+    Console::info('Call CreateHttpsBasicConfiguration success, response: ');
+    Console::info(Utils::toJSONString($createHttpsBasicConfigurationResponse));
+    return $createHttpsBasicConfigurationResponse->body;
+  }
+
+  /**
+   * @param CreateSiteResponseBody $siteResponseBody
+   * @param CreateHttpsBasicConfigurationResponseBody $createHttpsBasicConfigurationResponseBody
+   * @param ESA $client
+   * @return void
+   */
+  static public function updateHttpsCfg($siteResponseBody, $createHttpsBasicConfigurationResponseBody, $client)
+  {
+    Console::info('Begin Call UpdateHttpsBasicConfiguration to update resource');
+    $updateHttpsBasicConfigurationRequest = new UpdateHttpsBasicConfigurationRequest([
+      'siteId' => $siteResponseBody->siteId,
+      'ruleEnable' => 'on',
+      'https' => 'off',
+      'rule' => 'true',
+      'ruleName' => 'test2',
+      'configId' => $createHttpsBasicConfigurationResponseBody->configId,
+    ]);
+    $updateHttpsBasicConfigurationResponse = $client->updateHttpsBasicConfiguration($updateHttpsBasicConfigurationRequest);
+    Console::info('Call UpdateHttpsBasicConfiguration success, response: ');
+    Console::info(Utils::toJSONString($updateHttpsBasicConfigurationResponse));
+  }
+
+  /**
+   * @param CreateSiteResponseBody $siteResponseBody
+   * @param CreateHttpsBasicConfigurationResponseBody $createHttpsBasicConfigurationResponseBody
+   * @param ESA $client
+   * @return void
+   */
+  static public function destroyHttpsCfg($siteResponseBody, $createHttpsBasicConfigurationResponseBody, $client)
+  {
+    Console::info('Begin Call DeleteHttpsBasicConfiguration to destroy resource');
+    $deleteHttpsBasicConfigurationRequest = new DeleteHttpsBasicConfigurationRequest([
+      'siteId' => $siteResponseBody->siteId,
+      'configId' => $createHttpsBasicConfigurationResponseBody->configId,
+    ]);
+    $deleteHttpsBasicConfigurationResponse = $client->deleteHttpsBasicConfiguration($deleteHttpsBasicConfigurationRequest);
+    Console::info('Call DeleteHttpsBasicConfiguration success, response: ');
+    Console::info(Utils::toJSONString($deleteHttpsBasicConfigurationResponse));
+  }
+
+  /**
+   * @remarks
+   * Running code may affect the online resources of the current account, please proceed with caution!
+   * @param string[] $args
+   * @return void
+   */
+  static public function main($args)
+  {
+    // The code may contain api calls involving fees. Please ensure that you fully understand the charging methods and prices before running.
+    // Set the environment variable COST_ACK to true or delete the following judgment to run the sample code.
+    $costAcknowledged = getenv('COST_ACK');
+    if (is_null($costAcknowledged) || !$costAcknowledged === 'true') {
+      Console::warning('Running code may affect the online resources of the current account, please proceed with caution!');
+      return null;
+    }
+
+    // Init client
+    $esa20240910Client = self::createESA20240910Client();
+    // Init resource
+    //resource_RatePlanInstance_set_test
+    $ratePlanInstRespBody = self::ratePlanInst($esa20240910Client);
+    //resource_HttpBasicConfiguration_set_test
+    $siteRespBody = self::site($ratePlanInstRespBody, $esa20240910Client);
+    $httpsCfgRespBody = self::httpsCfg($siteRespBody, $esa20240910Client);
+    // update resource
+    self::updateHttpsCfg($siteRespBody, $httpsCfgRespBody, $esa20240910Client);
+    // destroy resource
+    self::destroyHttpsCfg($siteRespBody, $httpsCfgRespBody, $esa20240910Client);
+  }
+
+}
+$path = __DIR__ . \DIRECTORY_SEPARATOR . '..' . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
+if (file_exists($path)) {
+  require_once $path;
+}
+Sample::main(array_slice($argv, 1));
